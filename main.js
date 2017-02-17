bc29ee72a4578ef940f535c0e1c8980f2be7942b
Evan Limanto <evanlimanto@gmail.com>
2017-02-17 12:34:52 -0800

    add dependencies in package.json

--- main.js
+++ main.js
@@ -8,46 +8,5 @@ const _ = require('lodash')
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(express.static('./build'));
 console.log("server started on port " + port);
-
-const mjAPI = require("mathjax-node/lib/mj-page.js");
-mjAPI.start();
-
-app.get('/', function(req, res) {
-  glob('bank/fa15-q*.txt', {}, function (err, names) {
-    var html = '';
-    _.map(names, (name) => {
-      var contents = fs.readFileSync(name, 'utf8');
-      mjAPI.typeset({
-        html: contents,
-        render: "NativeMML",
-        inputs: ["TeX"],
-        xmlns: "mml"
-      }, function(result) {
-        html += result.html;
-        html += "<br/><br/>";
-      });
-    });
-    res.send(html);
-  });
-});
-
-app.get('/api', function(req, res) {
-  var id = req.query.id;
-  var filename = 'bank/' + id + '.txt';
-  try {
-    var contents = fs.readFileSync(filename, 'utf8');
-    mjAPI.typeset({
-      html: contents,
-      render: "NativeMML",
-      inputs: ["TeX"],
-      xmlns: "mml"
-    }, function(result) {
-      res.send(result.html);
-    });
-  } catch (e) {
-    res.status(404).send(e);
-  }
-});
-
-app.listen(port);
