58b67f251c0f5b53b880bf206ef40d5ef7b37292
elisexu <elisexu98@gmail.com>
2017-02-17 17:40:24 -0800

    add sp16 to midterm list

--- src/exams/index.js
+++ src/exams/index.js
@@ -1,4 +1,5 @@
 import EE16AFa15 from './ee16a-fa15';
 import EE16AFa16 from './ee16a-fa16';
+import EE16ASp16 from './ee16a-sp16';
 
-export { EE16AFa15, EE16AFa16 };
+export { EE16AFa15, EE16AFa16, EE16ASp16 };
