91cbbb7f21142ea7a0057aba204c36dd65ebc49e
elisexu <elisexu98@gmail.com>
2017-02-17 17:40:07 -0800

    add sp16 to midterm list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { EE16AFa15, EE16AFa16 } from './exams';
+import { EE16AFa15, EE16AFa16, EE16ASp16 } from './exams';
 import renderHTML from 'react-render-html';
 import { generateLatexMatrix } from './utils';
 
@@ -7,7 +7,7 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <EE16AFa16 />;
+    return <EE16ASp16/>;
   }
 }
 
