1f0d050b266044db6f59961f635a4a3bf327a3e9
Kevin TianXiang Wu <kevintxwu@gmail.com>
2017-02-19 15:54:55 -0800

    start of this shit

--- src/App.js
+++ src/App.js
@@ -28,6 +28,27 @@ class App extends Component {
     return (
       <span>
         <a className="return" href="/">&#8592; RETURN</a>
+        <a className="collapse">&#9776; COLLAPSE</a>
+        <div className="menu">
+          <a className="home center" href="/">Mavenform</a>
+          <hr className="s1" />
+          <h4>EE 16A</h4>
+          <hr className="s2" />
+          <div className="sidetitle">Midterm 1</div>
+          <div className="sidetab-container">
+            <a className="sidetab" href="/?exam=ee16afa16">Fall 2016</a>
+          </div>
+          <div className="sidetab-container">
+            <a className="sidetab" href="/?exam=ee16asp16">Spring 2016</a>
+          </div>
+          <div className="sidetab-container">
+            <a className="sidetab" href="/?exam=ee16afa15">Fall 2015</a>
+          </div>
+          <div className="sidetab-container">
+            <a className="sidetab" href="/?exam=ee16asp15">Spring 2015</a>
+          </div>
+          <a className="index" href="/">&#8592; RETURN</a>
+        </div>
         <a className="feedback" href="https://goo.gl/forms/JVXIpJ3TVhYNxMQW2" target="_blank">FEEDBACK?</a>
         <div className="test-container">
           <div className="test">
