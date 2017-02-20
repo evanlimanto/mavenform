720196c06d94408a2127b1fd1679ee83f27fc494
Kevin TianXiang Wu <kevintxwu@gmail.com>
2017-02-20 02:11:32 -0800

    table styling

--- src/course.js
+++ src/course.js
@@ -20,7 +20,7 @@ class Course extends Component {
           <hr className="margin-alt" />
           <h1 className="center">Exams</h1>
           <hr className="s1" />
-          <h5>Interactive, responsive, and navigable</h5>
+          <h5>Available Mavenform exams</h5>
           <hr className="s2" />
           <a className="card" href="/?exam=ee16afa16">
             <h2>Midterm 1</h2>
@@ -47,6 +47,66 @@ class Course extends Component {
             <h4 className="card-helper">CLICK TO VIEW &#8594;</h4>
           </a>
           <hr className="margin-alt" />
+        </div>
+        <div className="light-gray center">
+          <hr className="margin-alt" />
+          <h1 className="center">Sources</h1>
+          <hr className="s1" />
+          <h5>All publicly listed PDF exams</h5>
+          <hr className="s4" />
+          <div className="table-container-container">
+            <div className="table-container">
+              <table className="center">
+                <thead>
+                  <tr>
+                    <th>Type</th>
+                    <th>Term</th>
+                    <th>Instructors</th>
+                    <th>Exam</th>
+                    <th>Solutions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Midterm 1</td>
+                    <td>Fall 2016</td>
+                    <td>Ayazifar, Stojanovic</td>
+                    <td><a>PDF</a></td>
+                    <td><a>PDF</a></td>
+                  </tr>
+                  <tr>
+                    <td>Midterm 1</td>
+                    <td>Fall 2016</td>
+                    <td>Ayazifar, Stojanovic</td>
+                    <td><a>PDF</a></td>
+                    <td><a>PDF</a></td>
+                  </tr>
+                  <tr>
+                    <td>Midterm 1</td>
+                    <td>Fall 2016</td>
+                    <td>Ayazifar, Stojanovic</td>
+                    <td><a>PDF</a></td>
+                    <td><a>PDF</a></td>
+                  </tr>
+                  <tr>
+                    <td>Midterm 1</td>
+                    <td>Fall 2016</td>
+                    <td>Ayazifar, Stojanovic</td>
+                    <td><a>PDF</a></td>
+                    <td><a>PDF</a></td>
+                  </tr>
+                  <tr>
+                    <td>Midterm 1</td>
+                    <td>Fall 2016</td>
+                    <td>Ayazifar, Stojanovic</td>
+                    <td><a>PDF</a></td>
+                    <td><a>PDF</a></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <hr className="margin" />
+          </div>
         </div>
         <div className="banner center">
           <hr className="s2" />
