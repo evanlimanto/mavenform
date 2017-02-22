394f3379fc3e5014e515af05978e35abc8e8d342
Evan Limanto <evanlimanto@gmail.com>
2017-02-22 03:23:17 -0800

    Reorder cs61c

--- src/exams/consts.js
+++ src/exams/consts.js
@@ -31,30 +31,30 @@ const exams = {
         'profs': 'Boser, Katz',
         'url': '/exam?id=cs61cfa16',
       },
-      'Fall 2015': {
-        'id': 'cs61cfa15',
-        'profs': 'Stojanovic, Wawrzynek',
-        'url': '/exam?id=cs61cfa15',
-      },
       'Spring 2016': {
         'id': 'cs61csp16',
         'profs': 'Stojanovic, Weaver',
         'url': '/exam?id=cs61csp16',
+      },
+      'Fall 2015': {
+        'id': 'cs61cfa15',
+        'profs': 'Stojanovic, Wawrzynek',
+        'url': '/exam?id=cs61cfa15',
       },
       'Spring 2015': {
         'id': 'cs61csp15',
         'profs': 'Asanovic, Stojanovic',
         'url': '/exam?id=cs61csp15',
       },
-      'Spring 2014': {
-        'id': 'cs61csp14',
-        'profs': 'Garcia',
-        'url': '/exam?id=cs61csp14',
-      },
       'Fall 2014': {
         'id': 'cs61cfa14',
         'profs': 'Garcia, Lustig',
         'url': '/exam?id=cs61cfa14',
+      },
+      'Spring 2014': {
+        'id': 'cs61csp14',
+        'profs': 'Garcia',
+        'url': '/exam?id=cs61csp14',
       },
     },
   },
