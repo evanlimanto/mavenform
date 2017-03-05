7bb2044e0bb59e0dcba6cafb015c5a275ebce0ab
Evan Limanto <evanlimanto@gmail.com>
2017-03-04 22:04:42 -0800

    Build

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -18,7 +18,7 @@ class Navbar extends Component {
         <a className="logo" href="/" onClick={() => handleEvent('Click', 'Home - Desktop')}>Mavenform</a>
         <a className="material-icons mobile-back" href="/" onClick={() => handleEvent('Click', 'Home - Mobile')}>home</a>
         <div className="tooltip-container">
-          <a className="material-icons" href="https://docs.google.com/forms/d/e/1FAIpQLSfCS9McWikQ7F6syAGV9FX7Wf2-rWjqt-XMXxxEx5piTIf92Q/viewform?usp=sf_link">sms</a>
+          <a className="material-icons" href="https://docs.google.com/forms/d/e/1FAIpQLSfCS9McWikQ7F6syAGV9FX7Wf2-rWjqt-XMXxxEx5piTIf92Q/viewform?usp=sf_link" target="_blank">sms</a>
           <span className="tooltip">Send Feedback</span>
         </div>
         {appModeElement}
