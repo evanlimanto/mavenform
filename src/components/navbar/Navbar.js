9dd3f3823f3dc8bf3e3bc9dcd096b6e7b86531a1
Kevin TianXiang Wu <kevintxwu@gmail.com>
2017-03-04 22:12:39 -0800

    toggle all solutions button

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,7 +10,11 @@ class Navbar extends Component {
         <a className="logo" href="/" onClick={() => handleEvent('Click', 'Home - Desktop')}>Mavenform</a>
         <a className="material-icons mobile-back" href="/" onClick={() => handleEvent('Click', 'Home - Mobile')}>home</a>
         <div className="tooltip-container">
-          <a className="material-icons" href="https://docs.google.com/forms/d/e/1FAIpQLSfCS9McWikQ7F6syAGV9FX7Wf2-rWjqt-XMXxxEx5piTIf92Q/viewform?usp=sf_link">sms</a>
+          <a className="material-icons">done_all</a>
+          <span className="tooltip tooltip-long">Toggle All Solutions</span>
+        </div>
+        <div className="tooltip-container">
+          <a className="material-icons" href="https://docs.google.com/forms/d/e/1FAIpQLSfCS9McWikQ7F6syAGV9FX7Wf2-rWjqt-XMXxxEx5piTIf92Q/viewform?usp=sf_link" target="_blank">sms</a>
           <span className="tooltip">Send Feedback</span>
         </div>
         <div className="tooltip-container reader-mode" onClick={() => toggleAppModeCallback()}>
