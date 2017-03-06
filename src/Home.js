2a1cdfe1f2112165f5f3c5489bb590739ca74cf5
Kevin TianXiang Wu <kevintxwu@gmail.com>
2017-03-05 17:52:49 -0800

    updated home page

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,9 @@ import { handleEvent } from '../src/utils';
 import { courseIDToLabel, courses } from './exams';
 
 const _ = require('lodash');
+const Scroll = require('react-scroll');
+const Element = Scroll.Element;
+var Link = Scroll.Link;
 
 class Home extends Component {
   constructor(props) {
@@ -40,49 +43,72 @@ class Home extends Component {
           <div className="banner-text">
             <div className="banner-header">Mavenform</div>
             <hr className="s2" />
-            <div className="center">
-              <h5 className="h5-alt">A new and intuitive format to view and study past exams</h5>
+            <h5 className="h5-alt">A new and intuitive format to view and study past exams</h5>
+            <div className="home-button-container">
+              <Link
+               className="home-button"
+               to="courses"
+               isDynamic={true}
+               smooth={true}
+               duration={500}
+              >
+               View Courses
+              </Link>
+              <Link
+               className="home-button home-button-alt"
+               to="features"
+               isDynamic={true}
+               smooth={true}
+               duration={500}
+              >
+               Learn More
+              </Link>
             </div>
+            <img className="banner-screen" src="/img/screen.png"/> 
           </div>
         </div>
-        <div className="card-container center">
-          <hr className="margin" />
-          <h4 className="center">Courses</h4>
-          <hr className="s1" />
-          <h5>Currently available exam sets</h5>
-          <hr className="s3" />
-          {courseBoxes}
-          <hr className="margin" />
-        </div>
-        <div className="light-gray center">
-          <hr className="margin" />
-          <h4 className="center">Features</h4>
-          <hr className="s1" />
-          <h5>Why view exams in Mavenform?</h5>
-          <hr className="s3" />
-          <div className="column">
-            <div className="material-icons">touch_app</div>
-            <h1>Interactive</h1>
-            <hr className="s2" />
-            <p>Use interactable elements like solution toggling and a live table of contents to make it easier and faster to check your answers and find specific problems.</p>
+        <Element name="courses" className="card-container-container">
+          <div className="card-container center">
+            <hr className="margin" />
+            <h4 className="center">Courses</h4>
             <hr className="s1" />
+            <h5>Currently available exam sets</h5>
+            <hr className="s3" />
+            {courseBoxes}
+            <hr className="margin" />
           </div>
-          <div className="column">
-            <div className="material-icons">view_carousel</div>
-            <h1>Responsive</h1>
-            <hr className="s2" />
-            <p>No more need to zoom in and pan around. Unlike static document types, Mavenform is intuitive and legible with any device type and any screen width.</p>
+        </Element>
+        <Element name="features">
+          <div className="light-gray center">
+            <hr className="margin" />
+            <h4 className="center">Features</h4>
             <hr className="s1" />
+            <h5>Why view exams in Mavenform?</h5>
+            <hr className="s3" />
+            <div className="column">
+              <div className="material-icons">touch_app</div>
+              <h1>Interactive</h1>
+              <hr className="s2" />
+              <p>Use interactable elements like solution toggling and a live table of contents to make it easier and faster to check your answers and find specific problems.</p>
+              <hr className="s1" />
+            </div>
+            <div className="column">
+              <div className="material-icons">view_carousel</div>
+              <h1>Responsive</h1>
+              <hr className="s2" />
+              <p>No more need to zoom in and pan around. Unlike static document types, Mavenform is intuitive and legible with any device type and any screen width.</p>
+              <hr className="s1" />
+            </div>
+            <div className="column">
+              <div className="material-icons">navigation</div>
+              <h1>Navigable</h1>
+              <hr className="s2" />
+              <p>Instead of struggling to manage 10 or more tabs of individual PDFs, just browse a single web app that easily navigates between any relevant exam.</p>
+              <hr className="s1" />
+            </div>
+            <hr className="margin" />
           </div>
-          <div className="column">
-            <div className="material-icons">navigation</div>
-            <h1>Navigable</h1>
-            <hr className="s2" />
-            <p>Instead of struggling to manage 10 or more tabs of individual PDFs, just browse a single web app that easily navigates between any relevant exam.</p>
-            <hr className="s1" />
-          </div>
-          <hr className="margin" />
-        </div>
+        </Element>
         <div className="blue center">
           <hr className="s2" />
           <p className="white-text">Made by <a className="footer-link" href="http://www.kevinandstuff.com/" target="_blank">Kevin</a> & <a className="footer-link" href="http://evanlimanto.github.io/" target="_blank">Evan</a></p>
