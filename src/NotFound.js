dd334d9a79ee53fd0be7b60468cbcf935f352fdc
Kevin TianXiang Wu <kevintxwu@gmail.com>
2017-03-08 16:21:34 -0800

    now maven is a he

--- src/NotFound.js
+++ src/NotFound.js
@@ -8,7 +8,7 @@ class NotFound extends Component {
           <img className="maven-the-raven" src="/img/404.svg" />
           <hr className="s5" />
           <h1 className="center">Maven the Raven is lost...</h1>
-          We can't seem to find the page you're looking for.
+          He can't seem to find the page you're looking for.
           <hr className="s2" />
           <a className="error-link" href="/"> Return Home </a>
         </div>
