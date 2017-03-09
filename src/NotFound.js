9ce016f46663335e4147f9792a6303e6a831798b
Kevin TianXiang Wu <kevintxwu@gmail.com>
2017-03-08 16:17:32 -0800

    404 buddy

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class NotFound extends Component {
   render() {
-    return (<span>404 Page Here</span>);
+    return (
+      <div className="aligner">
+        <div className="aligner-item center">
+          <img className="maven-the-raven" src="/img/404.svg" />
+          <hr className="s5" />
+          <h1 className="center">Maven the Raven is lost...</h1>
+          We can't seem to find the page you're looking for.
+          <hr className="s2" />
+          <a className="error-link" href="/"> Return Home </a>
+        </div>
+      </div>
+    );
   }
 };
 
