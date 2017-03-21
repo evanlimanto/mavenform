c94766fac7062a48a3ab25c57286e691a0eca4c4
Evan Limanto <evanlimanto@gmail.com>
2017-03-20 19:01:42 -0700

    Add hotjar and inspectlet tracking

--- main.js
+++ main.js
@@ -12,8 +12,10 @@ const _ = require('lodash')
 const port = process.env.PORT || 8080;
 const app = express();
 
-const useCache = (process.env.NODE_ENV !== 'development');
+const useCache = false && (process.env.NODE_ENV !== 'development');
 const examCache = new NodeCache({ stdTTL: 30 * 60, checkperiod: 10 * 60 });
+
+console.log("Using redis to cache exams:", useCache);
 
 app.use('/img', express.static(path.join(__dirname, '/src/img')));
 
@@ -33,7 +35,7 @@ app.get('/getExam/:course/:type/:exam', function(req, res, next) {
     try {
       doc = fs.readFileSync(`src/exams/${course}/${type}-${exam}.yml`, "utf8");
       doc = yaml.safeLoad(doc);
-      error = error || !examCache.set(examKey, doc);
+      error = !examCache.set(examKey, doc);
     } catch(e) {
       console.log(e);
       error = true;
