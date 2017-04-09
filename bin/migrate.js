5572d4826f60abee8c2a219bd051d0fe026a1d04
Evan Limanto <evanlimanto@gmail.com>
2017-04-09 14:46:36 -0700

    Update migrate.js

--- bin/migrate.js
+++ bin/migrate.js
@@ -403,19 +403,54 @@ const config = {
   max: 5,
 };
 
-const Client = require('pg').Client;
+const prodConfig = {
+  user: 'wywhntmgwgishp',
+  database: 'd4de7agqe51r1g',
+  password: '99d4da5f646a2df74a609eb8fdea8a6fe4f084b8bf8d5dd7c796d9e8f48f0a14',
+  port: 5432,
+  host: 'ec2-23-21-96-70.compute-1.amazonaws.com',
+};
 
-const client = new Client(config);
+pg.defaults.ssl = true;
+const Client = pg.Client;
+
+const client = new Client(prodConfig);
 client.connect();
 
-  const q = `select problem_num, subproblem_num, problem, solution from exams where courseid = $1 and examtype = $2 and examid = $3`;
-  const courseid = 'cs61c';
-  const examtype = 'mt2';
-  const examid = 'fa15';
-  client.query({text: q, values: [courseid, examtype, examid]})
-    .then((result) => {
-      console.log(result)
-    });
+const courseid = 'cs61c';
+const examtype = 'mt1';
+const examid = 'fa16';
+
+const delq = `delete from exams where courseid = $1 and examtype = $2 and examid = $3`;
+client.query({text: delq, values: [courseid, examtype, examid]})
+  .then((result) => {
+    console.log(result)
+  });
+
+const contents = fs.readFileSync(`${examtype}-${examid}.yml`);
+let doc = null;
+try {
+  doc = yaml.safeLoad(contents);
+} catch (e) {
+  console.log(e);
+  process.exit(1);
+}
+const results = _.map(_.filter(_.keys(doc), function(k) {
+  return k.match(/^q\d_\d$/);
+}), (k) => [k, _.split(k.slice(1), "_")]);
+_.forEach(results, (res) => {
+  const problem_num = res[1][0];
+  const subproblem_num = res[1][1];
+  const problem = doc[res[0]];
+  const solution = doc[`${res[0]}_s`];
+  const q = `
+insert into exams (courseid, examtype, examid, problem_num, subproblem_num, problem, solution)
+values ($1, $2, $3, $4, $5, $6, $7)
+  `;
+  client.query(q, [courseid, examtype, examid, problem_num, subproblem_num, problem, solution], function (err, result) {
+    console.log(err, result);
+  });
+});
 
   /*
   _.forEach(exams, function (a, courseid) {
