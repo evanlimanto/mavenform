bf499a013a366a16772831405abd104c34875447
Evan Limanto <evanlimanto@gmail.com>
2017-06-20 21:51:16 -0700

    Bug fixes

--- src/components/school/School.js
+++ src/components/school/School.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { keys, has, map, sortBy, split, toInteger } from 'lodash';
+import { keys, has, map, sortBy, split, toInteger, takeRight } from 'lodash';
 import DocumentMeta from 'react-document-meta';
 
 import Footer from '../footer';
@@ -33,7 +33,7 @@ class SchoolComponent extends Component {
     const courseItems = (this.state.courses === null) ? "Loading courses..." : (
       (keys(this.state.courses).length > 0) ?
         map(sortBy(this.state.courses, [(course) => course.label]), (obj, subject) => {
-          const courseBoxes = map(sortBy(obj.courses, [(course) => toInteger((new RegExp("\\d+")).exec(course.code)[0])]), (course, key) => (
+          const courseBoxes = map(sortBy(obj.courses, [(course) => toInteger((new RegExp("\\d+")).exec(course.code)[0]), (course) => takeRight(course.code)]), (course, key) => (
             <Link className="card course-card" to={"/" + schoolCode + "/" + course.code} key={key} onClick={() => courseClickEvent(schoolCode, course.code)}>
               <span>{course.code_label}</span>
               <span className="card-arrow">&#8594;</span>
