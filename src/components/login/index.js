6255846b2986778482d65dfab804be1a20ee1247
Evan Limanto <evanlimanto@gmail.com>
2017-06-22 18:07:32 -0700

    Bug fixes

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,7 +1,6 @@
 import Login from './Login';
 import Logout from './Logout';
-import ForgotPassword from './ForgotPassword';
 import Signup from './Signup';
 import SecretSignup from './SecretSignup';
 
-export { Login, Logout, ForgotPassword, SecretSignup, Signup };
+export { Login, Logout, SecretSignup, Signup };
